2d59ac09620cd643a545df95463598c320803dc1
Xavier Tilley <hippietilley@gmail.com>
2018-06-04 14:12:23 -0500

    Documented FragmentMulitpleChoiceSelect

--- EMA/src/main/java/org/md2k/ema/fragment/FragmentMultipleChoiceSelect.java
+++ EMA/src/main/java/org/md2k/ema/fragment/FragmentMultipleChoiceSelect.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.md2k.ema.fragment;
 
 import android.os.Bundle;
@@ -19,34 +46,9 @@ import org.md2k.ema.R;
 
 import java.util.ArrayList;
 
-
 /**
- * Copyright (c) 2015, The University of Memphis, MD2K Center
- * - Syed Monowar Hossain <monowar.hossain@gmail.com>
- * All rights reserved.
- * <p/>
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * <p/>
- * * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- * <p/>
- * * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- * <p/>
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * UI fragment for multiple choice surveys.
  */
-
 public class FragmentMultipleChoiceSelect extends FragmentBase {
     private static final String TAG = FragmentMultipleChoiceSelect.class.getSimpleName();
     public static final String ITEM_UNSELECT_OTHER = "<UNSELECT_OTHER>";
@@ -54,17 +56,29 @@ public class FragmentMultipleChoiceSelect extends FragmentBase {
     TextView textViewPleaseSelect;
     ArrayAdapter<String> adapter;
 
+    /**
+     * Returns a <code>FragmentMultipleChoiceSelect</code> object with the given page number.
+     * @param pageNumber Page number for the new fragment.
+     * @return A <code>FragmentMultipleChoiceSelect</code> object with the given page number.
+     */
     public static FragmentMultipleChoiceSelect create(int pageNumber) {
         FragmentMultipleChoiceSelect fragment = new FragmentMultipleChoiceSelect();
         fragment.setArguments(getArgument(pageNumber));
         return fragment;
     }
 
+    /**
+     * Calls <code>super</code>.
+     * @param savedInstanceState Previous state of this activity, if it existed.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    /**
+     * Sets up the multiple choice response.
+     */
     void setTypeMultipleChoiceSelect() {
         if (question.isType(Constants.MULTIPLE_CHOICE))
             listView.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
@@ -89,10 +103,19 @@ public class FragmentMultipleChoiceSelect extends FragmentBase {
         for (int i = 0; i < question.getResponse_option().size(); i++)
             if (question.isResponseExist(question.getResponse_option().get(i)))
                 listView.setItemChecked(i, true);
-        else listView.setItemChecked(i, false);
-        if (question.isValid()) updateNext(true);
+        else
+            listView.setItemChecked(i, false);
+        if (question.isValid())
+            updateNext(true);
         else updateNext(false);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            /**
+             * Adds the response to the question when clicked.
+             * @param parent Parent view.
+             * @param view View clicked.
+             * @param position Position index.
+             * @param l Row id of the item clicked.
+             */
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
                 ArrayList<String> response = new ArrayList<>();
@@ -118,34 +141,56 @@ public class FragmentMultipleChoiceSelect extends FragmentBase {
                 question.setResponse(response);
                 if (question.isValid())
                     updateNext(true);
-                else updateNext(false);
-
+                else
+                    updateNext(false);
             }
         });
     }
 
+    /**
+     * Returns whether the question has been answered.
+     * @return Whether the question has been answered.
+     */
     public boolean isAnswered() {
-        return question.getQuestion_type() == null || !(question.getQuestion_type().equals(Constants.MULTIPLE_SELECT) ||
-                question.getQuestion_type().equals(Constants.MULTIPLE_CHOICE)) || question.getResponse().size() > 0;
+        return question.getQuestion_type() == null ||
+                !(question.getQuestion_type().equals(Constants.MULTIPLE_SELECT) ||
+                question.getQuestion_type().equals(Constants.MULTIPLE_CHOICE)) ||
+                question.getResponse().size() > 0;
     }
 
+    /**
+     * Calls <code>super</code> and <code>updateNext</code>.
+     * @param menu Menu to create.
+     * @param inflater Menu inflater.
+     */
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         updateNext(isAnswered());
     }
 
+    /**
+     * Creates the <code>rootView</code>.
+     * @param inflater Layout inflater.
+     * @param container View container.
+     * @param savedInstanceState Previous state of this activity, if it existed.
+     * @return The <code>rootView</code>
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final ViewGroup rootView = (ViewGroup) inflater
                 .inflate(R.layout.fragment_choice_select, container, false);
         listView = (ListView) rootView.findViewById(R.id.listView_options);
-        textViewPleaseSelect= (TextView) rootView.findViewById(R.id.textView_please_select);
+        textViewPleaseSelect = (TextView) rootView.findViewById(R.id.textView_please_select);
         setQuestionText(rootView, question);
         updateView();
         return rootView;
     }
+
+    /**
+     * Updates the view.
+     */
     public void updateView(){
         Log.d(TAG,"updateView()...");
         if (question.isType(Constants.MULTIPLE_CHOICE) || question.isType(Constants.MULTIPLE_SELECT)) {
@@ -153,7 +198,5 @@ public class FragmentMultipleChoiceSelect extends FragmentBase {
         } else {
             textViewPleaseSelect.setVisibility(View.GONE);
         }
-
     }
-
 }
